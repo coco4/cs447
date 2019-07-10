be5364ed076a23961ae7df95f1fb01394ff9e947
fseavey <fseavey@gmail.com>
2019-07-10 10:23:43 -0500

    Add navbar and pageheading to parent "App

---
+++ src/components/page-heading.vue
@@ -0,0 +1,17 @@
+<template>
+    <h1 class="text-center">{{title}}</h1>
+</template>
+
+<script>
+export default {
+    name: "PageHeading",
+    data(){
+        return {title: "Home Page"}
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
+
